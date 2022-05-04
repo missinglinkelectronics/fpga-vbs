f01e6aa2a12b5daf47c1837243ce12563b1c32cf
Joachim Foerster <joachim.foerster@missinglinkelectronics.com>
2022-05-04 18:00:27 +0200

    util/gen_bd_wrapper.tcl: Fix for block designs with hierarchy blocks and enhance informational output
    - Calling this script manually from the GUI for example, shall emit a reasonable
      error message. Note that this script is meant to be called via a filelist from
      within the MLE Vivado Build System's project creation phase.
    
    - Using get_bd_design and taking the first returned block design is sub-optimal.
      In designs with hierarchy blocks, not even with multiple real block designs,
      the first returned block design is NOT necessarily the desired block design.
      Instead one of the hierarchy blocks may be returned (naming scheme "bd_XXXX",
      where "X" is a hexadecimal digit).
    
      Using current_bd_design makes more sense, since the block design to create a
      wrapper for needs to be open anyway. So this script is supposed to be used in
      a filelist after a .tcl file creating a block design.
    
    - Finally, this commit adds more info output, to explicitly tell the user what
      is going on.
    
    Signed-off-by: Joachim Foerster <joachim.foerster@missinglinkelectronics.com>

--- util/gen_bd_wrapper.tcl
+++ util/gen_bd_wrapper.tcl
@@ -1,18 +1,37 @@
 # Generate Block Design wrapper
 
-if {![info exists bd_name]} {
-    set bd_name [lindex [get_bd_designs] 0]
+if {![info exists target_language]} {
+    puts "ERROR: Variable 'target_language' is not set"
+    puts "ERROR: Note that this utility script 'gen_bd_wrapper.tcl' is supposed to be executed from a filelist and witihn the MLE Vivado Build System and not manually!"
+    return 2
 }
+
+if {![info exists bd_name]} {
+    puts "INFO: Desired block design name to generate wrapper for is not set; thus trying to use current block design"
+    set bd_name [current_bd_design]
+}
+
+if {"${bd_name}" == ""} {
+    puts "ERROR: Unable to determine name of block design to generate wrapper for"
+    return 2
+}
+puts "INFO: Generating wrapper for block design '${bd_name}'"
 
 make_wrapper -files [get_files "${bd_name}.bd"] -top
 set bd_dir [file dirname [get_files "${bd_name}.bd"]]
 
 set bdw_name "${bd_name}_wrapper"
 if {"${target_language}" == "Verilog"} {
-    set bdw_name "${bdw_name}.v"
+    set bdw_filename "${bdw_name}.v"
 } else {
-    set bdw_name "${bdw_name}.vhd"
+    set bdw_filename "${bdw_name}.vhd"
 }
 
-add_files -norecurse [file normalize "${bd_dir}/hdl/${bdw_name}"]
-set_property top [file rootname [file tail "${bdw_name}"]] [current_fileset]
+add_files -norecurse [file normalize "${bd_dir}/hdl/${bdw_filename}"]
+
+set fileset [current_fileset]
+set top [get_property top ${fileset}]
+puts "INFO: Setting generated block design wrapper module '${bdw_name}' as top of fileset '${fileset}', replacing '${top}'"
+set_property top "${bdw_name}" [current_fileset]
+set top [get_property top ${fileset}]
+puts "INFO: Top of fileset '${fileset}' is set to '${top}'"
