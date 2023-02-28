1eea769eb519adfc683eb8ec744164644a6859e8
David Epping <david.epping@missinglinkelectronics.com>
2023-02-28 16:26:59 +0100

    helper.tcl: Keep stderr of git status when testing for git repo
    Signed-off-by: David Epping <david.epping@missinglinkelectronics.com>

--- helper.tcl
+++ helper.tcl
@@ -143,7 +143,7 @@ proc git_undo {old_branch new_branch level} {
 }
 
 proc git_exists {} {
-    if {[catch {exec git status -s >&/dev/null} result options]} {
+    if {[catch {exec git status -s 2>@1} result options]} {
         set ec [dict get $options -errorcode]
         if {[regexp {^POSIX ENOENT} $ec] == 1} {
             # assume no git executable found
